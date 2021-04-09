48a65cdc0078e39d59520ef9ce21da6930240366
Mdsomratakbor <somrat876@gmail.com>
2021-04-09 19:04:37 +0600

    Birthday-Cake-Candles problem solve

---
+++ Birthday-Cake-Candles/Birthday-Cake-Candles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Birthday_Cake_Candles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int candlesSize = Convert.ToInt32(Console.ReadLine());
+            string[] inputs = Console.ReadLine().Split(' ');
+            long[] arrayCandles = Array.ConvertAll(inputs, long.Parse);
+          Console.Write(MaxNumberPrint(arrayCandles));
+        }
+        static long MaxNumberPrint(long[] arr)
+        {
+            long maxNumber = arr.Max();
+            int output = 0;
+            for(int i=0; i<arr.Length; i++)
+            {
+                if(maxNumber== arr[i])
+                {
+                    output++;
+                }
+            }
+            return output;
+        }
+    }
+}
