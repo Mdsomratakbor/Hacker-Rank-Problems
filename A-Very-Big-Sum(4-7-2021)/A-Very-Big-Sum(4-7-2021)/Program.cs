8bdc5e3503387528236d52c63b3a5c74a5fc6c8b
Mdsomratakbor <somrat876@gmail.com>
2021-04-09 18:46:50 +0600

    mini and max number problem solve

---
+++ A-Very-Big-Sum(4-7-2021)/A-Very-Big-Sum(4-7-2021)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A_Very_Big_Sum_4_7_2021_
+{
+    class Program
+    {
+        static long SumArrayElement(int [] array)
+        {
+            long output=0;
+            for(int i=0; i<array.Length; i++)
+            {
+                output += array[i];
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            int arrayElement = Convert.ToInt32(Console.ReadLine());
+            string[] inputs = Console.ReadLine().Split(' ');
+            int i = 0;
+            int[] array = new int[arrayElement];
+            while (i<arrayElement)
+            {
+                array[i] = Convert.ToInt32(inputs[i]);
+                i++;
+            }
+            Console.WriteLine(SumArrayElement(array));
+        }
+    }
+}
---
+++ Mini-Max-Sum/Mini-Max-Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputsArray = Console.ReadLine().Split(' ');
+            //HACK :Old Style Code
+            //long[] arr = new long[5];
+
+            //for(int i=0; i<inputsArray.Length; i++)
+            //{
+            //    arr[i] = Convert.ToInt64(inputsArray[i]); 
+            //}
+
+            long[] arr = Array.ConvertAll(inputsArray, Int64.Parse);
+           var output =  SumMinMaxNumber(arr);
+            Console.Write("{0} {1}", output.Item1, output.Item2);
+
+        }
+        static Tuple<long, long> SumMinMaxNumber(long[] arr)
+        {
+            long[] arraySum = new long[5];
+            for (int i=0; i<arr.Length; i++)
+            {
+                long sum = 0;
+                for (int j=0;j<arr.Length; j++)
+                {
+                    sum += arr[j];
+                }
+                sum -= arr[i];
+                arraySum[i] = sum;
+            }
+            long minNumber = arraySum.Min();
+            long maxNumber = arraySum.Max();
+            return new Tuple<long, long>(minNumber,maxNumber);
+        }
+    }
+}
