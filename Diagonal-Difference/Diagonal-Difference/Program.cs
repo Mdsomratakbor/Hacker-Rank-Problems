78982d78921f1ece835fd0f2b1025c4f60ee2fda
Mdsomratakbor <somrat876@gmail.com>
2021-04-09 21:07:45 +0600

    Diagonal difference problem solve

---
+++ Diagonal-Difference/Diagonal-Difference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfArray = Convert.ToInt32(Console.ReadLine());
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < numberOfArray; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+            Console.WriteLine(diagonalDifference(arr));
+        }
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int[] result = { 0, 0 };
+            for(int i=0; i<arr.Count(); i++)
+            {
+                int a = arr[i][0 + i];
+                int b = arr[i][arr.Count() - 1 - i];
+                result[0] =  result[0] + a;
+                result[1] = result[1] + b;
+            }
+            return Math.Abs(result[0] - result[1]);
+        }
+    }
+}
--- Time-Conversion/Time-Conversion/Program.cs
+++ Time-Conversion/Time-Conversion/Program.cs
@@ -6,7 +6,17 @@ namespace Time_Conversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Console.Write(ToFormat24h(Convert.ToDateTime(input)));
+        }
+        //public static string ToFormat12h(DateTime dt)
+        //{
+        //    return dt.ToString("yyyy/MM/dd, hh:mm:ss tt");
+        //}
+
+        public static string ToFormat24h(DateTime dt)
+        {
+            return dt.ToString("HH:mm:ss");
         }
     }
 }
