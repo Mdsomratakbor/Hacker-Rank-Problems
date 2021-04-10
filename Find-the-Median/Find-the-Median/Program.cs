430c75475ff753060a10458b546934f29d46b9f3
Mdsomratakbor <somrat876@gmail.com>
2021-04-10 14:30:10 +0600

    find the median number

---
+++ Find-the-Median/Find-the-Median/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Find_the_Median
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int result = findMedian(arr);
+            Console.WriteLine(result);
+        }
+        // Complete the findMedian function below.
+        static int findMedian(int[] arr)
+        {
+             Array.Sort(arr);
+            decimal index = Math.Floor((decimal)arr.Length / 2);
+            return arr[(int)index];
+        }
+    }
+}
