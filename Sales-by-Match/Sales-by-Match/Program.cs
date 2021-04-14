fda1f995bb8681889c7a0caf61550ac31be4632d
Mdsomratakbor <somrat876@gmail.com>
2021-04-14 22:48:05 +0600

    sales by match problem solve

---
+++ Sales-by-Match/Sales-by-Match/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sales_by_Match
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+             sockMerchant(n, ar);
+
+        }
+        static void sockMerchant(int n, int[] ar)
+        {
+            int pairCount = 0;
+            List<int> newArray = new List<int>();
+            for(int i=0; i<ar.Length; i++)
+            {
+                if (!newArray.Contains(ar[i]))
+                {
+                    newArray.Add(ar[i]);
+                }
+                else
+                {
+                    pairCount++;
+                    newArray.Remove(ar[i]);
+                }
+            }
+            Console.WriteLine(pairCount);
+             
+
+        }
+    }
+}
