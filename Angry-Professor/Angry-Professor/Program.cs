3c5a40c460b408ae784396a5c624e4c6cb41f7eb
Mdsomratakbor <somrat876@gmail.com>
2021-04-16 19:01:36 +0600

    beautiful day problem solve

---
+++ Angry-Professor/Angry-Professor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Angry_Professor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            List<string> outputList = new List<string>();
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+                ;
+                outputList.Add(angryProfessor(k, a));
+              
+            }
+            foreach(var data in outputList)
+                Console.WriteLine(data);
+        }
+        static string angryProfessor(int k, int[] a)
+        {
+            string output = string.Empty;
+            int presentStudents = 0;
+            for(int i=0;i<a.Length; i++)
+            {
+                if (a[i] <= 0)
+                    presentStudents++;
+            }
+            if (presentStudents < k)
+                output = "YES";
+            else
+                output = "NO";
+            return output;
+        }
+    }
+}
---
+++ Beautiful-Days-at-the-Movies/Beautiful-Days-at-the-Movies/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Beautiful_Days_at_the_Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ijk = Console.ReadLine().Split(' ');
+
+            int i = Convert.ToInt32(ijk[0]);
+
+            int j = Convert.ToInt32(ijk[1]);
+
+            int k = Convert.ToInt32(ijk[2]);
+
+            int result = beautifulDays(i, j, k);
+            Console.WriteLine(result);
+        }
+        static int beautifulDays(int startDate, int endDate, int k)
+        {
+            int countBeautifulDays = 0;
+
+            for(int i = startDate; i<=endDate; i++)
+            {
+                int reverse = 0, beautifulDay = 0, day = i;
+                
+                while (day != 0)
+                {
+                    reverse = reverse * 10 + day % 10;
+                    day /= 10;
+                }
+                beautifulDay = Math.Abs(i-reverse);
+                if (beautifulDay % k == 0)
+                    countBeautifulDays++;
+                
+            }
+            return countBeautifulDays;
+        }
+    }
+}
