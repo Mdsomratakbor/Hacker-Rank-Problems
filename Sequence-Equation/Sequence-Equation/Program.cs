9f6b624ff972ab02357c6fdb0dedcfc8f5e75a91
Mdsomratakbor <somrat876@gmail.com>
2021-05-01 00:49:25 +0600

    Sequence Equation

---
+++ Sequence-Equation/Sequence-Equation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sequence_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> p = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(pTemp => Convert.ToInt32(pTemp)).ToList();
+
+            List<int> result = permutationEquation(p);
+            result.ForEach(x => Console.WriteLine(x));
+        }
+        public static List<int> permutationEquation(List<int> p)
+        {
+            List<int> output = new List<int>();
+            for (int i = 1; i <= p.Count; i++)
+                output.Add(p.IndexOf(p.IndexOf(i)+1) + 1);
+            return output;
+        }
+
+    }
+}
