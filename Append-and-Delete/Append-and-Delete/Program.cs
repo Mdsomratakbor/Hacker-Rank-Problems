48472e711b39b0acb45b9ecf495a4c24c2d0c8e2
Mdsomratakbor <somrat876@gmail.com>
2021-04-30 12:53:04 +0600

    some problem solve

---
+++ Append-and-Delete/Append-and-Delete/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Append_and_Delete
+{
+    class Program
+    {
+        // Complete the appendAndDelete function below.
+        static string appendAndDelete(string s, string t, int k)
+        {
+            int count=0, i;
+            if ((s.Length + t.Length) < k)
+                return "Yes";
+            for(i=0; i<Math.Min(s.Length, t.Length); i++)
+            {
+                if (s[i] == t[i])
+                    count++;
+                else
+                    break;
+            }
+             if ((k-s.Length-t.Length+2* count)%2==0) // Case 2
+                return "Yes";
+            return "No";
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string t = Console.ReadLine();
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            string result = appendAndDelete(s, t, k);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Extra-Long-Factorials/Extra-Long-Factorials/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace Extra_Long_Factorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            extraLongFactorials(n);
+        }
+        static void extraLongFactorials(int n)
+        {
+            var  fact = BigInteger.One;
+            for (int i = n; i > 0; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
