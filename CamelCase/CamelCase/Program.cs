f98eb02f53769fd86693ad21018d3609bdc4fdb7
Mdsomratakbor <somrat876@gmail.com>
2021-05-06 22:05:58 +0600

    camel case problem solve

---
+++ CamelCase/CamelCase/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int result = camelcase(s);
+            Console.WriteLine(result);
+        }
+        public static int camelcase(string s)
+        {
+            int count = 1;
+            for (int i = 0; i < s.Length; i++)
+                if (Char.IsUpper(s[i]))
+                    count++;
+            return count;
+        }
+
+    }
+}
