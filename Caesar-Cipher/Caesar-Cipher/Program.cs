86eb07d0e8a3803a9dd1002586c4f98eca75b12c
Mdsomratakbor <somrat876@gmail.com>
2021-05-07 17:57:13 +0600

    MARS exploration problem solve

---
+++ Caesar-Cipher/Caesar-Cipher/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string s = Console.ReadLine();
+
+            int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string result = caesarCipher(s, k);
+            Console.WriteLine(result);
+        }
+        public static string caesarCipher(string s, int k)
+        {
+            string output = string.Empty;
+            string orginalAlphabet = "abcdefghijklmnopqrstuvwxyz";
+            string substring = orginalAlphabet.Substring(0, k);
+            string roatedString = orginalAlphabet.Remove(0, k);
+            string roatedAlphabet = roatedString + substring;
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (Char.IsLetter(s[j]) == false)
+                {
+                    output = output + s[j];           
+                }
+                else
+                {
+                    for (int i = 0; i < orginalAlphabet.Length; i++)
+                    {
+                        if (Char.IsUpper(s[j]))
+                        {
+                            if (orginalAlphabet[i].ToString() == s[j].ToString().ToLower())
+                            {
+                                string upperCase = roatedAlphabet[i].ToString().ToUpper();
+                                output = output + upperCase;
+                                break;
+                            }
+                        }
+                        if (orginalAlphabet[i] == s[j])
+                        {
+                           
+                            output = output + roatedAlphabet[i];
+                            break;
+                        }
+                    }
+                }
+              
+            }
+            return output;
+        }
+
+    }
+}
---
+++ Mars-Exploration/Mars-Exploration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mars_Exploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            int result = marsExploration(s);
+            Console.WriteLine(result);
+        }
+        public static int marsExploration(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i += 3)
+            {
+                if (s[i] != 'S')
+                    count++;
+                if (s[i + 1] != 'O')
+                    count++;
+                if (s[i + 2] != 'S')
+                    count++;
+            }
+            return count;
+        }
+
+    }
+}
