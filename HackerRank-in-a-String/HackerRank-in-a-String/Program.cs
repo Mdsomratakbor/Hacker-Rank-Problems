c073f1e98d3670f53a3e95a42e7a4d2424d7bb6d
Mdsomratakbor <somrat876@gmail.com>
2021-05-20 22:17:04 +0600

    pangrms problem solve

---
+++ HackerRank-in-a-String/HackerRank-in-a-String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HackerRank_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                string result = hackerrankInString(s);
+
+                Console.WriteLine(result);
+            }
+            
+        }
+        public static string hackerrankInString(string s)
+        {
+            int index = 0, h = 1, a=2, c=3, k=4 , e=0, r=0,   
+        }
+    }
+}
---
+++ Pangrams/Pangrams/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = pangrams(s);
+            Console.WriteLine(result);
+        }
+        public static string pangrams(string s)
+        {
+            int count = 1;
+            s = s.Replace(" ", string.Empty);
+            s = s.ToLower();
+            var unique = new HashSet<char>(s);
+            if (unique.Count>=26)
+            {
+                foreach(var  value in unique)
+                {
+                    if(value== 'a' || value== 'b' || value== 'c' || value== 'd' || value== 'e' || value== 'f' || value== 'g' || value== 'h' || value== 'i' || value== 'j' || value== 'k' || value== 'l' || value== 'm' || value== 'n' || value== 'o' || value== 'p' || value== 'q' || value== 'r' || value== 's' || value== 't' || value== 'u' || value== 'v' || value== 'w' || value== 'x' || value== 'y' || value== 'z')
+                    {
+                        count++;
+                        if(count == 26)
+                        {
+                            return "pangram";
+                        }
+                    }
+                }
+            }
+           
+                return "not pangram";
+            
+        }
+    }
+}
